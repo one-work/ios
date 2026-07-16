a0b7dfbf636f831b7344e600ef52aa7962779ec5
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-16 22:10:44 +0800

    simple

--- Demo.swift
+++ Demo.swift
@@ -3,8 +3,7 @@ import Foundation
 struct Demo {
   static let remote = URL(string: "https://linlishenghuo.com")!
   static let local = URL(string: "http://localhost:3000")!
-  
-  /// Update this to choose which demo is run
+
   static var current: URL {
     remote
   }
