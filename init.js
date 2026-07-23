bb616dda3a14c0ec15ab47053dec2804f8a1b0ef
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-23 10:42:01 +0800

    fix

--- init.js
+++ init.js
@@ -1,14 +1,11 @@
 (() => {
   // 防止重复插入（Hotwire 跳转时可能会重复触发）
-  if (document.getElementById('my-injected-script')) return;
+  if (document.getElementById('my-injected-script')) return
 
-  const script = document.createElement('script');
-  script.id = 'my-injected-script';
-  script.src = 'https://your-cdn.com/my-script.js';
-  script.type = 'text/javascript';
-  
-  console.debug('ddd', document)
-  document.head.appendChild(script);
+  const script = document.createElement('script')
+  script.id = 'my-injected-script'
+  script.src = 'https://your-cdn.com/my-script.js'
+  document.head.appendChild(script)
 
   console.debug('注入 js  成功！')
 })()
