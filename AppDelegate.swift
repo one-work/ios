0f6c9f4a64c5172400329be2e57f470e8547adf0
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-24 00:08:27 +0800

    appdelete

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -4,7 +4,7 @@ import WebKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  
+
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     configureAppearance()
     configureHotwire()
