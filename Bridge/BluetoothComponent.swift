74a29634689377dfb2f98d01f2f09e354f22310a
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-29 17:33:49 +0800

    fix

--- Bridge/BluetoothComponent.swift
+++ Bridge/BluetoothComponent.swift
@@ -33,6 +33,10 @@ final class BluetoothComponent: BridgeComponent {
         }
       }
 
+      if (state != "connected") {
+        handleConnectDevice(message)
+      }
+      
       reply(to: "connect", with: ConnectStatus(address: address, name: name, state: state))
     case "search":
       BluetoothManager.shared.startScan()
