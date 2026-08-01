94fa1974eae9f661884641b757f54b9ce5b2b71a
qinmingyuan <mingyuan0715@foxmail.com>
2026-08-01 16:00:52 +0800

    improve code style

--- Demo.swift
+++ Demo.swift
@@ -3,7 +3,6 @@ import Foundation
 struct Demo {
   static let remote = URL(string: "https://linlishenghuo.com")!
   static let local = URL(string: "http://localhost:3000")!
-  static let example = URL(string: "https://app-demo.xcprinter.com")!
 
   static var current: URL {
     remote
